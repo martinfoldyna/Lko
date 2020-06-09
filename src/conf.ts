084e6738b39ec27323ecd527643476595d656e74
Martin Foldyna 1 <>
2020-06-09 09:43:46 +0200

    Dist folder

--- src/conf.ts
+++ src/conf.ts
@@ -4,9 +4,9 @@ export const Config = {
     scopes: ["user.read"]
   },
   google: {
-    providerID: '63183198040-t7kq60occjo8uqpc60bmasg6g0s2r42p.apps.googleusercontent.com', // Klíč pro Google přihlášení
-    // providerID: '752176834537-n6n0f6q82e1v69kv1q333fc7m0ubkue2.apps.googleusercontent.com', // PSLIB - Klíč pro Google přihlášení
+    // providerID: '63183198040-t7kq60occjo8uqpc60bmasg6g0s2r42p.apps.googleusercontent.com', // Klíč pro Google přihlášení
+    providerID: '752176834537-n6n0f6q82e1v69kv1q333fc7m0ubkue2.apps.googleusercontent.com', // PSLIB - Klíč pro Google přihlášení
   },
-  // apiUrl: 'https://lyceum.pslib.cloud/api/', // PSLIB - URL k api
-  apiUrl: 'http://localhost:3000/api/', // PSLIB - URL k api
+  apiUrl: 'https://lyceum.pslib.cloud/api/', // PSLIB - URL k api
+  // apiUrl: 'http://localhost:3000/api/', // LOCALHOST - URL k api
 }
